80f9dffccc767a2d827be7bff0a88f9dda7aeb7c
Vienio99 <kyve96@gmail.com>
2021-08-04 20:29:00 +0200

    added Comment model

--- social/models.py
+++ social/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from users.models import CustomUser
+from django.utils import timezone, dateformat
+
 
 # Create your models here.
 
 class Post(models.Model):
-  title = models.CharField(max_length=30)
-  author = models.ForeignKey(
-    CustomUser,
-    on_delete=models.CASCADE,
-  )
-  text = models.TextField()
-  pub_date = models.DateField()
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    pub_date = models.DateField(timezone.now())
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    pub_date = models.DateField(timezone.now())
--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 class CustomUser(AbstractUser):
-  age = models.PositiveIntegerField(null=True, blank=True)
-  hobby = models.CharField(blank=True, max_length=50)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    hobby = models.CharField(blank=True, max_length=50)
