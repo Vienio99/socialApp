952e224e5baee6d1828d502556bdaa8397e1bbb8
Vienio99 <kyve96@gmail.com>
2021-08-07 18:04:23 +0200

    created api endpoint for comments

--- api/v1/comment/views.py
+++ api/v1/comment/views.py
@@ -1,2 +1,11 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import AllowAny
 
+from social.models import Comment
+from .serializer import CommentSerializer
+
+
+class CommentApi(ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (AllowAny, )
