b5bdc638e5bafa0a0ed397629ec095474ff66d28
Vienio99 <kyve96@gmail.com>
2021-09-05 18:10:11 +0200

    fixed major problem with styles

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <div className="App">
+        <body className="fixed-header horizontal-menu horizontal-app-menu  pace-done">
+        </body>
+    </div>
+  );
+}
+
+export default Footer;
