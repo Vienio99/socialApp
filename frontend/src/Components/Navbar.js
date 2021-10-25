ab5dcd5b7f49f93337fdd12cb9e35492ae874c9c
Vienio99 <kyve96@gmail.com>
2021-10-25 22:55:23 +0200

    started to refactor code to tailwind - basic navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -5,24 +5,27 @@ import {
 
 export default function Navbar() {
     return (
+        <nav className="bg-gray-100">
+            <div className="container mx-auto text-center border max-w-6-xl">
+                <div className="flex justify-between">
+                    <div className="flex space-x-4">
+                        <div>
+                            <a href="#" className="flex">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none"
+                                     viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                          d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                </svg>
+                                <span>HobbyHub</span>
+                            </a>
+                        </div>
+                        <div>primary nav</div>
+                    </div>
+                    <div>secondary nav</div>
+                </div>
 
-    //    navbar goes here
-    <nav>
-        <div className="text-center font-bold">nav</div>
-    </nav>
-    //    logo
-
-    //    primary nav
-
-    //    secondary nav
-
-    //    mobile menu
-
-    //    content goes here
-    //     <div className="py-32 text-center">
-    //         <h2 className="font-extrabold text-6x1">Navbar</h2>
-    //     </div>
-
+            </div>
+        </nav>
     );
 }
 
