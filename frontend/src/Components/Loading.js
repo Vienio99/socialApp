ae61a8dc035ca446e36393287dccaed549017dbe
Vienio99 <kyve96@gmail.com>
2021-11-01 23:08:30 +0100

    changed logo icon

--- frontend/src/Components/Loading.js
+++ frontend/src/Components/Loading.js
@@ -3,10 +3,8 @@ import React from "react";
 
 function Loading() {
     return (
-        <div className="flex justify-center items-center">
-            <div
-                className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-600">
-            </div>
+        <div className="spinner">
+            fdasfsfdsaf
         </div>
     );
 }
