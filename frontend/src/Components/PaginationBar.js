504cb7b50edb3c2ae65ecb054179ebcf1a0b065e
Vienio99 <kyve96@gmail.com>
2021-11-03 11:29:46 +0100

    working next and previous buttons in PaginationBar

--- frontend/src/Components/PaginationBar.js
+++ frontend/src/Components/PaginationBar.js
@@ -38,10 +38,15 @@ export default function PaginationBar({postsPerPage, totalPosts, paginate, curre
                     <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
                         <a
                             href="#"
+                            onClick={ currentPage > 1 ? () => paginate(currentPage - 1) : null}
                             className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                         >
                             <span className="sr-only">Previous</span>
-                            <div className="h-5 w-5" aria-hidden="true"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M11 17l-5-5m0 0l5-5m-5 5h12"/>
+                            </svg>
                         </a>
                         {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
                         {pageNumbers.map(number => {
@@ -68,10 +73,15 @@ export default function PaginationBar({postsPerPage, totalPosts, paginate, curre
                         })}
                         <a
                             href="#"
+                            onClick={ currentPage < pageNumbers.length ? () => paginate(currentPage - 1) : null}
                             className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                         >
                             <span className="sr-only">Next</span>
-                            <div className="h-5 w-5" aria-hidden="true"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M13 7l5 5m0 0l-5 5m5-5H6"/>
+                            </svg>
                         </a>
                     </nav>
                 </div>
