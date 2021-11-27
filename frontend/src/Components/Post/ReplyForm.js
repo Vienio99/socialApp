45bf4188a44dcf5dcdf8f293a0b15281cf0a01e3
Vienio99 <kyve96@gmail.com>
2021-11-27 19:37:30 +0100

    ReplyForm refactored to Yup and React Form Hooks and tested

--- frontend/src/Components/Post/ReplyForm.js
+++ frontend/src/Components/Post/ReplyForm.js
@@ -1,36 +1,58 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import {replyPost} from "../../state/actions/posts";
 import {useDispatch} from "react-redux";
+import * as Yup from "yup";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+
+const schema = Yup.object().shape({
+        text: Yup.string()
+            .required('Text is required.')
+            .min(5, 'Text must be at least 5 characters long.')
+            .max(500, 'Text must not exceed 500 characters.'),
+    }
+);
 
 function ReplyForm(props) {
     // eslint-disable-next-line react/prop-types
     const {post} = props;
-    const [replyText, setReplyText] = useState('');
-
     const dispatch = useDispatch();
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors, isSubmitSuccessful}
+    } = useForm({resolver: yupResolver(schema)});
 
     // Reply logic
-    const handleReply = (e) => {
-        e.preventDefault();
-        dispatch(replyPost(replyText, post.id));
-        console.log(replyText);
+    const submitForm = (data) => {
+        dispatch(replyPost(data.text, post.id));
+        console.log(data.text);
         // Clear form after submitting
-        setReplyText('');
     };
+
+    useEffect(() => {
+        if (isSubmitSuccessful) {
+            reset();
+        }
+    }, [isSubmitSuccessful, reset]);
+
+
     return (
         <div className="flex flex-col w-3/4 ml-auto">
             <div className="flex justify-end">
                 <div className="flex flex-col w-1/2 px-4 py-4 mb-4 bg-gray-200 shadow-md rounded-md">
-                    <form onSubmit={e => handleReply(e)}>
+                    <form onSubmit={handleSubmit(submitForm)}>
                         <div className="mb-4">
                             <input
                                 className="w-full px-3 py-2 text-sm text-gray-700 border rounded shadow appearance-none"
                                 id="text"
                                 type="text" placeholder="Text"
-                                onChange={e => setReplyText(e.target.value)}
-                                value={replyText}
+                                {...register('text')}
                             />
+                            {errors.text &&
+                            <p className="px-3 mt-2 text-xs italic text-red-500">{errors.text.message}</p>}
                         </div>
                         <div className="flex items-center justify-end">
                             <button
--- frontend/src/__tests__/ReplyForm.test.js
+++ frontend/src/__tests__/ReplyForm.test.js
@@ -8,14 +8,14 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "../state/store";
-import PostForm from "../Components/Forms/PostForm";
+import ReplyForm from "../Components/Post/ReplyForm";
 
 
 beforeEach(() => {
     render(
         <Provider store={store}>
             <BrowserRouter>
-                <PostForm/>
+                <ReplyForm/>
             </BrowserRouter>
         </Provider>
     );
@@ -23,39 +23,32 @@ beforeEach(() => {
 
 const setUp = () => {
     const text = screen.queryByPlaceholderText(/Text/i);
-    const tags = screen.queryByPlaceholderText(/#tags/i);
-    const submitButton = screen.getByText('Send');
+    const submitButton = screen.getByText('Reply');
 
     return {
         text,
-        tags,
         submitButton
     };
 };
 
 test('renders empty form properly', () => {
-    const {text, tags, submitButton} = setUp();
+    const {text, submitButton} = setUp();
 
     expect(text).toBeInTheDocument();
-    expect(tags).toBeInTheDocument();
     expect(submitButton).toBeInTheDocument();
 
     expect(text).toHaveValue('');
-    expect(tags).toHaveValue('');
-
 });
 
 test('input fields are blank after submitting the form', async () => {
-    const {text, tags, submitButton} = setUp();
+    const {text, submitButton} = setUp();
 
     await waitFor(() => {
         fireEvent.change(text, {target: {value: 'Test text'}});
-        fireEvent.change(tags, {target: {value: '#hiking'}});
         fireEvent.click(submitButton);
     });
 
     expect(text).toHaveValue('');
-    expect(tags).toHaveValue('');
 });
 
 test('displays required error messages', async () => {
@@ -66,78 +59,51 @@ test('displays required error messages', async () => {
     });
 
     const textError = screen.queryByText('Text is required.');
-    const tagsError = screen.queryByText('Tags are required.');
 
     expect(textError).toBeInTheDocument();
-    expect(tagsError).toBeInTheDocument();
 });
 
 test('displays minimum length error messages', async () => {
-    const {text, tags, submitButton} = setUp();
+    const {text, submitButton} = setUp();
 
     await waitFor(() => {
         fireEvent.change(text, {target: {value: 'min'}});
-        fireEvent.change(tags, {target: {value: '#gn'}});
 
         fireEvent.click(submitButton);
     });
 
     const textError = screen.queryByText('Text must be at least 5 characters long.');
-    const tagsError = screen.queryByText('Tags have to be at least 5 characters long.');
 
     expect(textError).toBeInTheDocument();
-    expect(tagsError).toBeInTheDocument();
 });
 
 test('displays maximum length error messages', async () => {
-    const {text, tags, submitButton} = setUp();
+    const {text, submitButton} = setUp();
 
     await waitFor(() => {
         fireEvent.change(text, {target: {value: 'lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text lenghty text '}});
-        fireEvent.change(tags, {target: {value: '#long #tags #long #tags #long #tags #long #tags #long #tags #long #tags #long #tags #long #tags #long'}});
-
         fireEvent.click(submitButton);
     });
 
     const textError = screen.queryByText('Text must not exceed 500 characters.');
-    const tagsError = screen.queryByText('Tags must not exceed 100 characters.');
 
     expect(textError).toBeInTheDocument();
-    expect(tagsError).toBeInTheDocument();
-});
-
-test('displays improper tags format error', async () => {
-    const {text, tags, submitButton} = setUp();
-
-    await waitFor(() => {
-        fireEvent.change(text, {target: {value: 'Correct text'}});
-        fireEvent.change(tags, {target: {value: 'incorrect #tags'}});
-        fireEvent.click(submitButton);
-    });
-
-    const tagsError = screen.queryByText('Proper format for tags is f.e. #hiking');
-
-    expect(tagsError).toBeInTheDocument();
 });
 
 test('removes error messages if input is valid', async () => {
-    const {text, tags, submitButton} = setUp();
+    const {text, submitButton} = setUp();
 
     await waitFor(() => {
         fireEvent.click(submitButton);
     });
 
     const textError = screen.queryByText('Text is required.');
-    const tagsError = screen.queryByText('Tags are required.');
 
     expect(textError).toBeInTheDocument();
-    expect(tagsError).toBeInTheDocument();
 
     await waitFor(() => {
         fireEvent.change(text, {target: {value: 'Correct text'}});
-        fireEvent.change(tags, {target: {value: '#correct #tags'}});
     });
 
     expect(textError).not.toBeInTheDocument();
-    expect(tagsError).not.toBeInTheDocument();
 });
